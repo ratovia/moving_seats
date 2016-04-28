20f09ad2ef93d3b9f214963f05ac1e3fbf468b97
ratovia <ratovia@hotmail.co.jp>
2016-04-29 02:02:11 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function all_lottery(){
+  $.getJSON("js/names.json" , function(data){
+    for(var i = 1;i < 7;i++) {
+      for(var j = 1;j < 7;j++){
+        var idx = getRandomInt(1,data.length);
+        var value = data[idx];
+        data.splice( idx, idx);
+        $("'#td' + i + '-' + j").text(value);
+      }
+    }
+  });
+};
+
+function create_table(){
+  var masume;
+  for(var i = 1; i < 7;i++){
+    masume += "<tr>";
+    for(var j = 1;j < 7;j++){
+      masume += "<td id='td" + i + "-" + j + "'></td>";
+    }
+    masume += "</tr>"
+  }
+  $("#table").append(masume);
+}
+
+function personal_lottery(){
+
+}
+
+window.addEventListener("load",function(eve){
+  create_table();
+  all_lottery();
+},false);
+
+$("#all_lottery").click(function(){
+  all_lottery();
+});
+
+$("#personal_lottery").click(function(){
+  personal_lottery();
+});
